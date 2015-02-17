3e6766b304884005e3b093f42da57421b6f28dc0
Toby Weston <toby.weston@gmail.com>
2015-02-17 22:41:28 +0000

    testing JNI

---
+++ ws2812/swig_example/Test.java
@@ -0,0 +1,10 @@
+	public class Test {
+	public static void main(String... args) {
+
+
+		System.out.println("******************************************************");
+		System.out.println("* Current time via JNI is :    " + example.get_time());
+		System.out.println("******************************************************");
+	}
+	}
+
