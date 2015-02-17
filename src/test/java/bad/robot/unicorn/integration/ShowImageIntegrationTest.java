bb3d654237512fd5998727e901d68d4d3e56016d
Toby Weston <toby.weston@gmail.com>
2015-02-17 21:52:10 +0000

    run test against just one image

--- src/test/java/bad/robot/unicorn/integration/ShowImageIntegrationTest.java
+++ src/test/java/bad/robot/unicorn/integration/ShowImageIntegrationTest.java
@@ -4,6 +4,7 @@ import bad.robot.unicorn.Coordinate;
 import bad.robot.unicorn.SpriteSheet;
 import bad.robot.unicorn.StubUnicorn;
 import bad.robot.unicorn.Unicorn;
+import bad.robot.unicorn.neopixel.NeoPixelDisplayMatrix;
 import org.junit.Test;
 
 import java.awt.*;
@@ -28,14 +29,14 @@ public class ShowImageIntegrationTest {
         SpriteSheet sheet = new SpriteSheet(16, 18, 8, 8);
         BufferedImage[] images = sheet.subImages("/lofi.png");
 
-		Unicorn unicorn = new StubUnicorn();
-
-        Arrays.stream(images).forEach(image -> {
+		Unicorn unicorn = new NeoPixelDisplayMatrix();
+		BufferedImage image = images[0];
+//        Arrays.stream(images).forEach(image -> {
             Stream<Coordinate> _8x8 = range(0, 8).boxed().flatMap(x -> range(0, 8).mapToObj(y -> new Coordinate(x, y)));
 			_8x8.forEach(coordinate -> unicorn.setPixelColor(coordinate.x, coordinate.y, new Color(image.getRGB(coordinate.x, coordinate.y))));
 			unicorn.show();
 			sleep(500, MILLISECONDS);
-		});
+//		});
     }
 
 }
