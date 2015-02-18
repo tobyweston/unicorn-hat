1f54f66b50a7f90211f117b3e7a03d1bd8f4c4bd
toby.weston <toby.weston@db.com>
2015-02-18 14:55:56 +0000

    help diagnose lib path problems when running in maven

--- src/test/java/bad/robot/unicorn/integration/jni/LdLibraryPathTest.java
+++ src/test/java/bad/robot/unicorn/integration/jni/LdLibraryPathTest.java
@@ -2,13 +2,28 @@ package bad.robot.unicorn.integration.jni;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+
 public class LdLibraryPathTest {
 
 	@Test
 	public void sharedObjectShouldLoad() {
-		System.out.println("******************************************************");
-		System.out.println("* Current time via JNI is :    " + example.get_time());
-		System.out.println("******************************************************");
+        String time = example.get_time();
+        System.out.println("*******************************************************");
+        System.out.println("* Current time via JNI is :    " + time);
+		System.out.println("*******************************************************");
 	}
+
+    @Test
+    public void canSeeEnvironmentVariablesFromTest() {
+        String path = System.getenv("LD_LIBRARY_PATH");
+        assertThat(path, is(not(nullValue())));
+        System.out.println("*******************************************************");
+        System.out.println("* LD_LIBRARY_PATH as seen by the test : " + path);
+        System.out.println("*******************************************************");
+    }
 
 }
