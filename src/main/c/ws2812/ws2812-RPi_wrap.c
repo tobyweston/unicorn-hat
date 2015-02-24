84cd46729d06fb841080a792cfd2fed87d0fc6b1
Toby Weston <toby.weston@gmail.com>
2015-02-24 19:35:34 +0000

    updated wrapper C file after moving lib files around

--- src/main/c/ws2812/ws2812-RPi_wrap.c
+++ src/main/c/ws2812/ws2812-RPi_wrap.c
@@ -187,7 +187,7 @@ static void SWIGUNUSED SWIG_JavaThrowException(JNIEnv *jenv, SWIG_JavaExceptionC
 #define SWIG_contract_assert(nullreturn, expr, msg) if (!(expr)) {SWIG_JavaThrowException(jenv, SWIG_JavaIllegalArgumentException, msg); return nullreturn; } else
 
 
-#include "ws2812-RPi.h"
+#include "lib/ws2812-RPi.h"
 
 
 #ifdef __cplusplus
