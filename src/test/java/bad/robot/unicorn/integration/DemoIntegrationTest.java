f932c08e9fe63cbd68a86ab34b43b46c0b990ce3
Toby Weston <toby.weston@gmail.com>
2015-02-26 20:29:17 +0000

    colour tweak

--- src/test/java/bad/robot/unicorn/integration/DemoIntegrationTest.java
+++ src/test/java/bad/robot/unicorn/integration/DemoIntegrationTest.java
@@ -15,12 +15,12 @@ import static java.util.stream.IntStream.range;
 
 public class DemoIntegrationTest {
 
-    @Test
-    public void swirlyGoodness() {
+	@Test
+	public void swirlyGoodness() {
 		swirlyGoodness(new Ws2811Unicorn());
-    }
+	}
 
-    private void swirlyGoodness(Unicorn unicorn) {
+	private void swirlyGoodness(Unicorn unicorn) {
 		Swirl effect = new Swirl();
 		unicorn.setBrightness(0.05);
 
@@ -29,10 +29,10 @@ public class DemoIntegrationTest {
 				range(0, 8).forEach(y -> {
 					range(0, 8).forEach(x -> {
 						RawColour colour = effect.getColourFor(x, y, step);
-						int red = (int) Math.max(0, Math.min(255, colour.red));
-						int green =  (int) Math.max(0, Math.min(255, colour.green));
-						int blue =  (int) Math.max(0, Math.min(255, colour.blue));
-						unicorn.setPixelColor(x, y, new Color(red, green, blue));
+						double red = Math.max(0, Math.min(255, colour.red));
+						double green = Math.max(0, Math.min(255, colour.green));
+						double blue = Math.max(0, Math.min(255, colour.blue));
+						unicorn.setPixelColor(x, y, new Color(Color.HSBtoRGB((float) red, (float) green, (float) blue)));
 					});
 				});
 				// step
@@ -47,43 +47,43 @@ public class DemoIntegrationTest {
 		new DemoIntegrationTest().swirlyGoodness(createUnicorn(args));
 	}
 
-    interface Effect {
-        RawColour getColourFor(int x, int y, int step);
-    }
+	interface Effect {
+		RawColour getColourFor(int x, int y, int step);
+	}
 
-    public class Swirl implements Effect {
+	public class Swirl implements Effect {
 
-        public RawColour getColourFor(int x, int y, int step) {
-            x -= 4;
-            y -= 4;
+		public RawColour getColourFor(int x, int y, int step) {
+			x -= 4;
+			y -= 4;
 
-            double dist = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) - 2.0;
-            double angle = (step / 10.0) + (dist * 1.5);
-            double s = Math.sin(angle);
-            double c = Math.cos(angle);
+			double dist = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) - 2.0;
+			double angle = (step / 10.0) + (dist * 1.5);
+			double s = Math.sin(angle);
+			double c = Math.cos(angle);
 
-            double xs = x * c - y * s;
-            double ys = x * s - y * c;
+			double xs = x * c - y * s;
+			double ys = x * s - y * c;
 
-            double r = Math.abs(xs + ys);
-            r *= 64.0;
-            r -= 20;
+			double r = Math.abs(xs + ys);
+			r *= 64.0;
+			r -= 20;
 
-            return new RawColour(r, r + (s * 130), r + (c * 130));
-        }
-    }
+			return new RawColour(r, r + (s * 130), r + (c * 130));
+		}
+	}
 
-    class RawColour {
-        final double red;
-        final double green;
-        final double blue;
+	class RawColour {
+		final double red;
+		final double green;
+		final double blue;
 
-        public RawColour(double red, double green, double blue) {
-            this.red = red;
-            this.green = green;
-            this.blue = blue;
-        }
+		public RawColour(double red, double green, double blue) {
+			this.red = red;
+			this.green = green;
+			this.blue = blue;
+		}
 
-    }
+	}
 
 }
