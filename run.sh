079bd7b3991b04b1d87df9bc0d72164bbedf6d4d
Toby Weston <toby.weston@gmail.com>
2015-02-26 19:44:53 +0000

    update library path for ws2811

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo java -cp target/test-classes:target/classes/ -Djava.library.path=/home/pi/dev/unicorn-hat-java/src/main/c/ws2812 bad.robot.unicorn.integration.$1IntegrationTest
+sudo java -cp target/test-classes:target/classes/ -Djava.library.path=/home/pi/dev/unicorn-hat-java/src/main/c/ws2812:/home/pi/dev/unicorn-hat-java/src/main/c/rpi-ws281x bad.robot.unicorn.integration.$1IntegrationTest
--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo java -cp target/test-classes:target/classes/ -Djava.library.path=/home/pi/dev/unicorn-hat-java/src/main/c/ws2812 bad.robot.unicorn.integration.TurnOnSinglePixel $1 $2
+sudo java -cp target/test-classes:target/classes/ -Djava.library.path=/home/pi/dev/unicorn-hat-java/src/main/c/ws2812:/home/pi/dev/unicorn-hat-java/src/main/c/rpi-ws281x bad.robot.unicorn.integration.TurnOnSinglePixel $1 $2
