2043cbdb1e04dfcfe17369b2328207aa7d0104d1
Toby Weston <toby.weston@gmail.com>
2015-02-26 19:59:11 +0000

    nada

--- src/test/java/bad/robot/unicorn/integration/OrientationIntegrationTest.java
+++ src/test/java/bad/robot/unicorn/integration/OrientationIntegrationTest.java
@@ -1,20 +1,16 @@
 package bad.robot.unicorn.integration;
 
-import bad.robot.unicorn.Orientation;
 import bad.robot.unicorn.Unicorn;
 import bad.robot.unicorn.neopixel.ws2811.Ws2811Unicorn;
-import bad.robot.unicorn.neopixel.ws2812.Ws2812Unicorn;
 import bad.robot.unicorn.shape.Arrow;
 import bad.robot.unicorn.shape.Shape;
 import org.junit.Test;
 
-import java.awt.*;
 import java.util.stream.Stream;
 
 import static bad.robot.unicorn.Orientation.*;
 import static bad.robot.unicorn.Sleep.sleep;
-import static bad.robot.unicorn.integration.CommandLine.*;
-import static java.awt.Color.*;
+import static bad.robot.unicorn.integration.CommandLine.createUnicorn;
 import static java.awt.Color.green;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
